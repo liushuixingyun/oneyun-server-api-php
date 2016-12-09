c2392657abe005f86b7044b1ac5c36a12396017e
root <root@d01.yunhuni.cn>
2016-12-09 11:21:30 +0800

    commit size oneyun Oneyun

--- oneyun/Exceptions/Exception.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Oneyun\Exception;
-
-class OneyunException extends \Exception
-{
-
-}
--- oneyun/Http/Client.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Oneyun\Http;
-
-interface Client
-{
-    public function request($method, $url, $params = array(), $data = array(),
-                            $headers = array(), $timeout = null);
-
-}
